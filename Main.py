d53c868d4d5e18c4033556856fdf94ff8579fb8e
Daniel McCave <mccavedaniel@icloud.com>
2026-03-10 13:21:59 -0700

    Update Main.py
    Updated code to correctly update total once another number is entered by user, changed the grade variable to update only when the total is within average score,

--- Main.py
+++ Main.py
@@ -1,24 +1,26 @@
 print("Student Grade Calculator")
 subject_count = int(input("Enter the number of subjects:"))
+while subject_count <= 0:
+    print("Invalid number of subjects. Please enter a positive number.")
+    subject_count = int(input("Please enter a positive number between 0 and 100: "))
 print(f"You have {subject_count} subjects.")
 total_score = 0
 print(f"Total so far: {total_score}")
 for i in range(subject_count):
     score = float(input("Enter score for subject: "))
-    print(f"Total so far: {total_score}")
     total_score = total_score + score
-    average_score = total_score / subject_count
+    print(f"Total so far: {total_score}")
+average_score = total_score / subject_count
 print(f"Your average score is: {average_score:.2f}")
 print(f"Total score: {total_score}")
-grade = average_score
-if grade >= 90 and grade <= 100:
-    print("A")
-elif grade >= 80 and grade <= 89.99:
-    print("B")
-elif grade >= 70 and grade <= 79.99:
-    print("C")
-elif grade >= 60 and grade <= 69.99:
-    print("D")
+if (total_score >= 90):
+    Grade = "A"
+elif (total_score >= 80):
+    Grade = "B"
+elif (total_score >= 70):
+    Grade = "C"
+elif (total_score >= 60):
+    grade = "D"
 else:
     print("F")
-print(f"Your grade is: {grade}")
+print(f"Your grade is: {Grade}")
