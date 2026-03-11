ecc0d11db43f90f2f074cedf9fbf88c8759c7039
Daniel McCave <mccavedaniel@icloud.com>
2026-03-10 17:07:30 -0700

    Update Main.py
    updated main to include a list to store names of subjects.
    add a for loop to ask how many subjects and what are the names.
    Added a report card to show user a more organized final version

--- Main.py
+++ Main.py
@@ -1,26 +1,48 @@
 print("Student Grade Calculator")
-subject_count = int(input("Enter the number of subjects:"))
-while subject_count <= 0:
+print("=" * 20)
+subject_count = int(input("Enter the number of subjects: "))
+subjects = [] # Store the subject names in a list.
+
+if subject_count <= 0:
     print("Invalid number of subjects. Please enter a positive number.")
-    subject_count = int(input("Please enter a positive number between 0 and 100: "))
+    subject_count = int(input("Enter the number of subjects: "))
+else:
+    print("Great! now lets enter the name of the subjects!")
+
 print(f"You have {subject_count} subjects.")
-total_score = 0
-print(f"Total so far: {total_score}")
 for i in range(subject_count):
-    score = float(input("Enter score for subject: "))
-    total_score = total_score + score
-    print(f"Total so far: {total_score}")
+    subjects.append(input(f"Enter the name of subject {i+1}: "))
+print(input("Ready to continue? (yes or no): "))
+if input().lower() != "yes":
+    print("Great lets get the scores for the subjects!")
+else:
+    print(input("Okay whats the name of the next subject?: "))
+
+
+total_score = 0
+
+for i in range(subject_count):
+    score = int(input(f"Enter score for subject {i+1}: "))
+    while score < 0 or score > 100:
+        print("Invalid score. Please enter a value between 0 and 100.")
+        score = int(input(f"Enter score for subject {i+1}: "))
+
+    total_score += score
+
 average_score = total_score / subject_count
-print(f"Your average score is: {average_score:.2f}")
-print(f"Total score: {total_score}")
-if (total_score >= 90):
-    Grade = "A"
-elif (total_score >= 80):
-    Grade = "B"
-elif (total_score >= 70):
-    Grade = "C"
-elif (total_score >= 60):
+
+if average_score >= 90:
+    grade = "A"
+elif average_score >= 80:
+    grade = "B"
+elif average_score >= 70:
+    grade = "C"
+elif average_score >= 60:
     grade = "D"
 else:
-    print("F")
-print(f"Your grade is: {Grade}")
+    grade = "F"
+
+print("--- Student Report ---")
+print(f"Total Score: {total_score}")
+print(f"Average Score: {average_score:.2f}")
+print(f"Grade: {grade}")
